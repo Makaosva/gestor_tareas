ba229bcf92e3eeedaf4002de1c77629d5be13880
Alejandro Gasparotto <alejandrogasparotto182@gmail.com>
2024-05-29 23:39:11 -0400

    feat/Agregamos task con el uso de Hook UseState

--- src/components/TaskManager.jsx
+++ src/components/TaskManager.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import {useState} from 'react'
+import TaskForm from './TaskForm'
+import TaskList from './TaskList'
 
 function TaskManager() {
-  return (
-    <div>TaskManager</div>
+
+const [tasks, setTasks] = useState([])/* arreglo */; 
+const addTask = (task)=>{
+  setTasks([...tasks, task])/* al arreglo task agregale un task */
+}
+    return (
+    <div>
+      <h1>Task Manager</h1>
+      <TaskForm addTask = {addTask}/>
+      <TaskList tasks = {tasks}/>
+    </div>
   )
 }
 
